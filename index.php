1a425d99957f9de6edf473d9f6296a14b7a7b424
nicolecadiz <77320186+nicolecadiz@users.noreply.github.com>
2021-03-01 20:40:53 +0800

    Rename TLE PT.html to index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+﻿<html>
+<head>
+      <title> TLE PT </title>
+</head> 
+<style>
+body {
+  background-image: url('sunset.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+<font size="70" color="red"><b><center> TODAY </center></b></font>
+</body>
+
+	<table border="1" bgcolor="orange" bordercolor="black" cellspacing="3" cellpadding="15">
+    <tr>
+        <th>ISSUES</th>
+    </tr>
+    <tr>
+        <td align="center">LOCAL NEWS</td>
+        <td align="center">The Philippine government said it would prosecute a police officer over the deaths of a mother 
+and son, after a video of the point blank shooting went viral in social media, prompting renewed calls against police 
+violence and extrajudicial killings.
+
+Senior Master Sergeant Jonel Nuezca is facing double murder charges over the death of Sonya Gregorio and her son, Frank 
+Anthony, who were Nuezca’s neighbors in Tarlac province north of the capital, according to the police report. The officer 
+has surrendered to authorities, it said.</td>
+    </tr>
+    <tr>
+        <td align="center">SPORTS</td>
+        <td align="center">Brooklyn Nets guard Kyrie Irving is leading the charge to compel the NBA to change its shield 
+logo to Kobe Bryant, and he has the backing of the late great's widow, Vanessa Bryant.
+
+In a post to his Instagram page Wednesday, Irving presented an image of Kobe overlayed on the NBA shield, ostensibly his 
+choice to supplant the silhouette image modeled after Jerry West, used as the NBA logo since 1969.</td>
+    <tr>
+	<td align="center">HEALTH</td>
+	<td align="center">The Department of Health's (DOH) advisory group has approved the use of Sinovac's COVID-19 
+vaccine for Philippine healthcare workers but said those who prefer another brand would not be penalized.
+
+The interim National Immunization Technical Advisory Group (NITAG) made the announcement in a virtual press briefing 
+hosted by the DOH on Friday afternoon, February 26.</td>
+    </tr>
+    <tr>
+	<td align="center">WORLD NEWS</td>
+	<td align="center">An intermediate magnitude 4.2 earthquake was reported early morning near Ashkāsham, Ishkāshim, 
+Badakhshan, Afghanistan.
+According to the United States Geological Survey, the quake hit on Saturday 27 February 2021 at 1:35 am local time at an 
+intermediate depth of 106.9 km. The strength of the earthquake may have been tempered by its relative great depth below 
+the surface, which makes it feel weaker in absolute terms. The exact magnitude, epicenter, and depth of the quake might be 
+revised within the next few hours or minutes as seismologists review data and refine their calculations, or as other 
+agencies issue their report.
+A second report was later issued by the European-Mediterranean Seismological Centre (EMSC), which listed it as a magnitude 
+4.2 earthquake as well.
+Towns or cities near the epicenter where the quake might have been felt as very weak shaking include Ashkāsham (pop. 
+12,100) located 30 km from the epicenter, Ishqoshim (pop. 26,000) 37 km away, and Jurm (pop. 12,100) 71 km away.</td>
+    </tr>
+</table>
+<br>
+<body>
+<font size="70" color="blue"><b><center> TOMORROW </center></b></font>
+<p align="center">
+<img src="image of hope.png">
+<br>
+<br>
+<center>
+   <a href="https://www.youtube.com/watch?v=Cn4rp5gXtLU&feature=share&fbclid=IwAR08GGMHlPps3En7rNxe0uz4pqMHJq59_hvGeJYtGwLu6rUhBeE-OzM1hNw"><b><font size="50"> DON'T LOSE HOPE(click this)</a>
+</center>
+</body>
+</html>
